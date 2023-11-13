b221313897735f4d8285568bb5a964a4ba264266
Fulvio Di Maio <fulvio.dimaio1@gmail.com>
2023-11-13 14:32:56 +0100

    fixed errors

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,12 +1,13 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.actions";
-import { auth } from "@clerk/nextjs";
+// import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  const { userId } = auth();
+  // const { userId } = auth();
 
+  const userId = "saghaghagh";
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
