16d717602d7c3837de75a13c799ae8245929f706
Fulvio Di Maio <fulvio.dimaio1@gmail.com>
2023-11-13 14:11:26 +0100

    turn of vercel auth

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,17 +1,16 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.actions";
-// import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  // const { userId } = auth();
+  const { userId } = auth();
 
-  const userId = "ABCD1234";
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
-  console.log(mongoUser);
+
   return (
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
