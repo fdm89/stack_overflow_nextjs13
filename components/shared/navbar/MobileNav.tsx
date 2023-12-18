7dfd78969fc174909cc027c0d98ec0548253d4c7
Fulvio Di Maio <fulvio.dimaio1@gmail.com>
2023-12-18 15:36:54 +0100

    ...

--- components/shared/navbar/MobileNav.tsx
+++ components/shared/navbar/MobileNav.tsx
@@ -55,9 +55,9 @@ const MobileNav = () => {
       <SheetTrigger asChild>
         <Image
           src="/assets/icons/hamburger.svg"
-          alt="menu"
           width={36}
           height={36}
+          alt="Menu"
           className="invert-colors sm:hidden"
         />
       </SheetTrigger>
@@ -70,8 +70,9 @@ const MobileNav = () => {
             src="/assets/images/site-logo.svg"
             width={23}
             height={23}
-            alt="Devflow"
+            alt="DevFlow"
           />
+
           <p className="h2-bold text-dark100_light900 font-spaceGrotesk">
             Dev <span className="text-primary-500">Overflow</span>
           </p>
@@ -86,13 +87,14 @@ const MobileNav = () => {
               <SheetClose asChild>
                 <Link href="/sign-in">
                   <Button className="small-medium btn-secondary min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
-                    <span className="primary-text-gradient">Log in</span>
+                    <span className="primary-text-gradient">Log In</span>
                   </Button>
                 </Link>
               </SheetClose>
+
               <SheetClose asChild>
                 <Link href="/sign-up">
-                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
+                  <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg border px-4 py-3 shadow-none">
                     Sign Up
                   </Button>
                 </Link>
